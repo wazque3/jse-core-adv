ca9afac416f79997d01d0e2e2c0aa9bdaa3eeee0
wazquez <wazqu3z@gmail.com>
2021-05-11 15:50:59 -0500

    Added person and car models

---
+++ src/com/wazquez/arrays/Car.java
@@ -0,0 +1,42 @@
+package com.wazquez.arrays;
+
+import java.util.Arrays;
+
+public class Car {
+     private int id;
+     private String brand;
+     private String model;
+     private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
